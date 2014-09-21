5ab750e3044b55549bf2b21ada46df815a016103
Maik Riechert <maik.riechert@arcor.de>
2014-09-21 23:47:12 +0200

    add (slightly big) test image, wrap interpolation methods

--- _rawpy.pyx
+++ _rawpy.pyx
@@ -11,15 +11,20 @@ np.import_array()
 cdef extern from "libraw.h":
     ctypedef unsigned short ushort
     
-    struct libraw_image_sizes_t:
+    cdef enum LibRaw_image_formats:
+        LIBRAW_IMAGE_JPEG
+        LIBRAW_IMAGE_BITMAP
+    
+    ctypedef struct libraw_image_sizes_t:
         ushort raw_height, raw_width
-        ushort height, width 
+        ushort height, width
         ushort top_margin, left_margin
+        ushort iheight, iwidth
 
-    struct libraw_rawdata_t:
+    ctypedef struct libraw_rawdata_t:
         ushort *raw_image
 
-    struct libraw_data_t:
+    ctypedef struct libraw_data_t:
         ushort                      (*image)[4]
         libraw_image_sizes_t        sizes
 #         libraw_iparams_t            idata
@@ -32,6 +37,12 @@ cdef extern from "libraw.h":
         libraw_rawdata_t            rawdata
 #         void                *parent_class
 
+    ctypedef struct libraw_processed_image_t:
+        #enum LibRaw_image_formats type
+        ushort height, width, colors, bits
+        unsigned int  data_size 
+        unsigned char data[1] # this is the image data, no idea why [1]
+
     cdef cppclass LibRaw:
         libraw_data_t imgdata
         LibRaw()
@@ -41,6 +52,9 @@ cdef extern from "libraw.h":
         int raw2image()
         int raw2image_ex(int do_subtract_black)
         void raw2image_start()
+        int dcraw_process()
+        libraw_processed_image_t* dcraw_make_mem_image(int *errcode=NULL)
+        void dcraw_clear_mem(libraw_processed_image_t* img)
         void free_image()
         
 def enum(**enums):
@@ -61,6 +75,7 @@ cdef class RawPy:
         del self.p
     
     def open_file(self, path):
+        # TODO check error code and turn into exception
         self.p.open_file(_chars(path))
         
     def unpack(self):
@@ -68,17 +83,22 @@ cdef class RawPy:
         
     @property
     def rawdata(self):
+        """Bayer-pattern RAW image, one channel."""
         cdef ushort* raw = self.p.imgdata.rawdata.raw_image
         cdef np.npy_intp shape[2]
         shape[0] = <np.npy_intp> self.p.imgdata.sizes.raw_height
         shape[1] = <np.npy_intp> self.p.imgdata.sizes.raw_width
         return np.PyArray_SimpleNewFromData(2, shape, np.NPY_USHORT, raw)
     
+    def raw2image(self):
+        self.p.raw2image()
+    
     @property
     def image(self):
+        """RAW composite image with channels RGBR."""
         cdef np.npy_intp shape[2]
-        shape[0] = <np.npy_intp> self.p.imgdata.sizes.height
-        shape[1] = <np.npy_intp> self.p.imgdata.sizes.width
+        shape[0] = <np.npy_intp> self.p.imgdata.sizes.iheight
+        shape[1] = <np.npy_intp> self.p.imgdata.sizes.iwidth
         return [np.PyArray_SimpleNewFromData(2, shape, np.NPY_USHORT, self.p.imgdata.image[0]),
                 np.PyArray_SimpleNewFromData(2, shape, np.NPY_USHORT, self.p.imgdata.image[1]),
                 np.PyArray_SimpleNewFromData(2, shape, np.NPY_USHORT, self.p.imgdata.image[2]),
@@ -87,8 +107,20 @@ cdef class RawPy:
     def subtract_black(self):
         self.p.subtract_black()
         
-    def raw2image(self):
-        self.p.raw2image()
+    def dcraw_process(self):
+        self.p.dcraw_process()
+        
+    def dcraw_make_mem_image(self):
+        # TODO how can it be that unsigned char represent 16 bits?
+        cdef libraw_processed_image_t* img = self.p.dcraw_make_mem_image()
+        #assert img.type == LIBRAW_IMAGE_BITMAP
+        cdef np.npy_intp shape[3]
+        shape[0] = <np.npy_intp> img.height
+        shape[1] = <np.npy_intp> img.width
+        shape[2] = <np.npy_intp> img.colors
+        # FIXME memory leak, numpy doesn't own img.data! we have to free it later using dcraw_clear_mem()!
+        #       -> write wrapper class with __array__
+        return np.PyArray_SimpleNewFromData(3, shape, np.NPY_UINT8 if img.bits == 8 else np.NPY_UINT16, img.data)
         
 cdef char* _chars(s):
     if isinstance(s, unicode):
--- test/tests.py
+++ test/tests.py
@@ -1,16 +1,27 @@
 from __future__ import division, print_function
 
+import os
 import numpy as np
 from numpy.testing.utils import assert_array_equal
 
 import rawpy
 
+rawTestPath = os.path.join(os.path.dirname(__file__), 'iss030e122639.NEF')
+
 def testFileOpen():
-    raw = rawpy.imread(r'C:\Users\maik\Desktop\iss\iss030e122539.NEF')
+    raw = rawpy.imread(rawTestPath)
     assert_array_equal(raw.rawdata.shape, [2844, 4288])
     print(np.min(raw.rawdata), np.max(raw.rawdata))
+    
+    # composite RAW image with RGBR channels
     raw.raw2image()
-    # TODO what is the 4th channel??
     for img_channel in raw.image:
         print(img_channel.shape)
         print(np.min(img_channel), np.max(img_channel), np.mean(img_channel))
+    
+    # run bayer interpolation and get the RGB image
+    raw.dcraw_process()
+    rgb = raw.dcraw_make_mem_image()
+    print(rgb.dtype, rgb.shape)
+    for i in range(rgb.shape[2]):
+        print(np.min(rgb[:,:,i]), np.max(rgb[:,:,i]), np.mean(rgb[:,:,i]))
