14a9838acad5307af5534bed80ba0450532b84dc
Maik Riechert <maik.riechert@arcor.de>
2014-10-01 20:29:06 +0200

    fix pypi deploy script

--- appveyor/pypi_deploy.ps1
+++ appveyor/pypi_deploy.ps1
@@ -1,6 +1,6 @@
 $gitinfo = git log -n 1 --pretty=format:'%d'
 echo "git log: $gitinfo"
-if (($gitinfo -match "master") -and ($gitinfo -match "tag: v")) {
+if ($gitinfo -match "tag: v") {
   (Get-Content appveyor\.pypirc) | Foreach-Object {$_ -replace '%PASS%',$env:PYPI_PASS} | Set-Content $env:userprofile\.pypirc
   Invoke-Expression "$env:CMD_IN_ENV python setup.py bdist_wheel upload"
 }
