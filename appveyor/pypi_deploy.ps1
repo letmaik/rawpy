2e622bbe99e8b6e9d6aa5cbba6bef07e0aa23cdd
Maik Riechert <maik.riechert@arcor.de>
2014-10-03 08:34:28 +0200

    use new appveyor git tag feature for deployment

--- appveyor/pypi_deploy.ps1
+++ appveyor/pypi_deploy.ps1
@@ -1,6 +1,4 @@
-$gitinfo = git log -n 1 --pretty=format:'%d'
-echo "git log: $gitinfo"
-if ($gitinfo -match "tag: v") {
+if (($env:appveyor_repo_tag -eq "True") -and ($env:appveyor_repo_branch.StartsWith("v"))) {
   (Get-Content appveyor\.pypirc) | Foreach-Object {$_ -replace '%PASS%',$env:PYPI_PASS} | Set-Content $env:userprofile\.pypirc
   Invoke-Expression "$env:CMD_IN_ENV python setup.py bdist_wheel upload"
 }
