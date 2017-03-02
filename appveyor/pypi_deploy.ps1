f18c4f33a67754f86b48e50fdeecbb109bacffb5
Maik Riechert <maik.riechert@arcor.de>
2017-03-02 19:33:46 +0000

    fix appveyor deploy script

--- appveyor/pypi_deploy.ps1
+++ appveyor/pypi_deploy.ps1
@@ -1,7 +1,7 @@
 echo "APPVEYOR_REPO_TAG: " + $env:APPVEYOR_REPO_TAG
-echo "APPVEYOR_REPO_BRANCH: " + $env:APPVEYOR_REPO_BRANCH
+echo "APPVEYOR_REPO_TAG_NAME: " + $env:APPVEYOR_REPO_TAG_NAME
 
-if (($env:APPVEYOR_REPO_TAG -eq "True") -and ($env:APPVEYOR_REPO_BRANCH.StartsWith("v"))) {
+if (($env:APPVEYOR_REPO_TAG -eq "True") -and ($env:APPVEYOR_REPO_TAG_NAME.StartsWith("v"))) {
   (Get-Content appveyor\.pypirc) | Foreach-Object {$_ -replace '%PASS%',$env:PYPI_PASS} | Set-Content $env:userprofile\.pypirc
   Invoke-Expression "$env:CMD_IN_ENV python setup.py bdist_wheel upload"
 }
