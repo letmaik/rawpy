593223d8d668d4e46af04deaea564795b60481a2
Maik Riechert <maik.riechert@arcor.de>
2017-05-29 14:52:11 +0100

    lib versions

--- travis/build.sh
+++ travis/build.sh
@@ -61,16 +61,12 @@ travis_retry yum install -y lapack-devel blas-devel
 
 # Install python dependencies
 for PYBIN in ${PYBINS[@]}; do
-    # skimage requires numpy >= 1.7.2, so we use that as our minimum supported numpy version
+    # numpy 1.7 is our minimum supported version
     ${PYBIN}/pip install numpy==1.7.2
 
-    # scipy 0.18.1 is the first version with x86 & x64 manylinux1 wheels for py 27,34,35,36
-    ${PYBIN}/pip install scipy==0.18.1
-    
-    # use older mpl because newer versions require a newer freetype version which doesn't exist on CentOS 5
-    ${PYBIN}/pip install matplotlib==1.3.1
-
-    # newest version that still supports numpy 1.7.2
+    # the following versions still support numpy 1.7
+    ${PYBIN}/pip install scipy==0.18.1    
+    ${PYBIN}/pip install matplotlib==2.0.3
     ${PYBIN}/pip install scikit-image==0.12.3
         
     ${PYBIN}/pip install -r /io/dev-requirements.txt
