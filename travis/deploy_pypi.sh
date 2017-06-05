edaf86f5c23ab6680ff05e9641c7c098c99a5a4b
Maik Riechert <maik.riechert@arcor.de>
2017-06-05 22:57:12 +0100

    fix mac

--- travis/deploy_pypi.sh
+++ travis/deploy_pypi.sh
@@ -5,6 +5,8 @@ if [[ "`git log -n 1 --pretty=format:'%d'`" =~ "tag: v" ]]; then
     pip install twine
     twine upload -u letmaik -p $PYPI_PASS dist/*
     if [[ "$TRAVIS_OS_NAME" == "osx" && "$PYTHON_VERSION" == "2.7" ]]; then
+        # re-install jpeg, gets removed before testing, see build_macos.sh
+        brew install jpeg
         python setup.py build_ext --inplace
         python setup.py build_sphinx
         python setup.py upload_docs
