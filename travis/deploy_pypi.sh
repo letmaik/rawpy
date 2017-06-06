59940a8cd9268eab3e8290930de3cb0c3e15e8c2
Maik Riechert <maik.riechert@arcor.de>
2017-06-06 22:39:48 +0100

    more cleanup

--- travis/deploy_pypi.sh
+++ travis/deploy_pypi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e -x
 
-if [[ "`git log -n 1 --pretty=format:'%d'`" =~ v[0-9]+\.[0-9]+\.[0-9]+ ]]; then
+if [[ "$TRAVIS_TAG" =~ v[0-9]+\.[0-9]+\.[0-9]+ ]]; then
     pip install twine
-    twine upload -u letmaik -p $PYPI_PASS --skip-existing dist/*
+    twine upload -u $PYPI_USER -p $PYPI_PASS --skip-existing dist/*
 fi
