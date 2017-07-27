c3d107dc48f09bbe86f1152b4f5ab1a65835b8d0
Maik Riechert <maik.riechert@arcor.de>
2017-07-27 11:55:48 +0100

    deploy docs to github pages

--- appveyor/pypi_deploy.ps1
+++ appveyor/pypi_deploy.ps1
@@ -9,11 +9,4 @@ if (($env:APPVEYOR_REPO_TAG -eq "True") -and ($env:APPVEYOR_REPO_TAG_NAME.Starts
   
   # build and upload binary wheel
   Invoke-Expression "$env:CMD_IN_ENV python setup.py bdist_wheel upload"
-  
-  # build and upload docs
-  if ($env:PYTHON -eq "C:\Python36_64") {
-    Invoke-Expression "$env:CMD_IN_ENV python setup.py build_ext --inplace"
-    Invoke-Expression "python setup.py build_sphinx"
-    Invoke-Expression "python setup.py upload_docs"
-  }
 }
--- travis/deploy_pypi.sh
+++ travis/deploy_pypi.sh
@@ -4,4 +4,10 @@ set -e -x
 if [[ "$TRAVIS_TAG" =~ v[0-9]+\.[0-9]+\.[0-9]+ ]]; then
     pip install twine
     twine upload -u $PYPI_USER -p $PYPI_PASS --skip-existing dist/*
+
+    if [[ "$TRAVIS_OS_NAME" == "osx" && "$PYTHON_VERSION" = "3.6" ]]; then
+        python setup.py build_ext --inplace
+        python setup.py build_sphinx
+        # upload via deploy options in .travis.yml
+    fi
 fi
