9ae5bbb574c2f7411402d7aa98d731b47147fea7
Maik Riechert <maik.riechert@arcor.de>
2017-07-27 20:18:03 +0100

    fix docs building

--- travis/deploy_pypi.sh
+++ travis/deploy_pypi.sh
@@ -6,6 +6,8 @@ if [[ "$TRAVIS_TAG" =~ v[0-9]+\.[0-9]+\.[0-9]+ ]]; then
     twine upload -u $PYPI_USER -p $PYPI_PASS --skip-existing dist/*
 
     if [[ "$TRAVIS_OS_NAME" == "osx" && "$PYTHON_VERSION" = "3.6" ]]; then
+        # re-install libjpeg as it was removed before running tests (see build_macos.sh)
+        brew install jpeg
         python setup.py build_ext --inplace
         python setup.py build_sphinx
         # upload via deploy options in .travis.yml
