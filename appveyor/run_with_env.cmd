8d094fa8996fc2cb7d01cbb099678e0c984766cb
Maik Riechert <maik.riechert@arcor.de>
2019-09-21 15:37:54 +0100

    fix

--- appveyor/run_with_env.cmd
+++ appveyor/run_with_env.cmd
@@ -17,6 +17,7 @@ python --version
 
 SET COMMAND_TO_RUN=%*
 SET VS2015_ROOT=C:\Program Files (x86)\Microsoft Visual Studio 14.0
+SET VS2017_ROOT=C:\Program Files (x86)\Microsoft Visual Studio\2017
 
 :: Extract the major and minor versions, and allow for the minor version to be
 :: more than 9.  This requires the version number to have two dots in it.
@@ -28,7 +29,16 @@ IF "%PYTHON_VERSION:~3,1%" == "." (
 )
 
 IF %MAJOR_PYTHON_VERSION% == 3 (
-    SET VS_VERSION="2015"
+	IF EXIST %VS2017_ROOT% (
+		SET VS_VERSION="2017"
+		IF EXIST %VS2017_ROOT%\Enterprise (
+			SET VS2017_ROOT=%VS2017_ROOT%\Enterprise
+		) ELSE (
+			SET VS2017_ROOT=%VS2017_ROOT%\Community
+		)
+	) ELSE (
+		SET VS_VERSION="2015"
+	)
 ) ELSE (
     ECHO Unsupported Python version: "%MAJOR_PYTHON_VERSION%"
     EXIT 1
@@ -40,11 +50,21 @@ IF "%PYTHON_ARCH%"=="64" (
 	    "%VS2015_ROOT%\VC\vcvarsall.bat" x64
 	    ECHO Executing: %COMMAND_TO_RUN%
 		call %COMMAND_TO_RUN% || EXIT 1
+	) ELSE IF %VS_VERSION%=="2017" (
+	    ECHO Configuring VS2017 for Python %MAJOR_PYTHON_VERSION%.%MINOR_PYTHON_VERSION% on a 64 bit architecture
+	    "%VS2017_ROOT%\Common7\Tools\vsdevcmd.bat" -arch=x64
+	    ECHO Executing: %COMMAND_TO_RUN%
+		call %COMMAND_TO_RUN% || EXIT 1
 	)
 ) ELSE (
 	IF %VS_VERSION%=="2015" (
 	    ECHO Configuring VS2015 for Python %MAJOR_PYTHON_VERSION%.%MINOR_PYTHON_VERSION% on a 32 bit architecture
 	    "%VS2015_ROOT%\VC\vcvarsall.bat" x86
+	    ECHO Executing: %COMMAND_TO_RUN%
+		call %COMMAND_TO_RUN% || EXIT 1
+	) ELSE IF %VS_VERSION%=="2017" (
+	    ECHO Configuring VS2017 for Python %MAJOR_PYTHON_VERSION%.%MINOR_PYTHON_VERSION% on a 32 bit architecture
+	    "%VS2017_ROOT%\Common7\Tools\vsdevcmd.bat" -arch=x86
 	    ECHO Executing: %COMMAND_TO_RUN%
 		call %COMMAND_TO_RUN% || EXIT 1
 	)
