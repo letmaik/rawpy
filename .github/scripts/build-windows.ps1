7735857608a681e63c5b36ffbbd483190ebfae69
Maik Riechert <maik.riechert@arcor.de>
2022-02-24 21:50:51 +0000

    ci: add visual studio 2022 support

--- .github/scripts/build-windows.ps1
+++ .github/scripts/build-windows.ps1
@@ -69,7 +69,7 @@ function Initialize-VS {
     # https://docs.microsoft.com/en-us/cpp/porting/binary-compat-2015-2017
 
     $VS_ROOT = "C:\Program Files (x86)\Microsoft Visual Studio"
-    $VS_VERSIONS = @("2017", "2019")
+    $VS_VERSIONS = @("2017", "2019", "2022")
     $VS_EDITIONS = @("Enterprise", "Professional", "Community")
     $VS_INIT_CMD_SUFFIX = "Common7\Tools\vsdevcmd.bat"
 
