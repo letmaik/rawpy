1cf3bfcc7fef30f088933148ecf1d995ddaf14fe
Maik Riechert <letmaik@outlook.com>
2026-02-10 08:42:49 +0000

    re-enable parallel build on linux (libraw)

--- setup.py
+++ setup.py
@@ -258,7 +258,7 @@ def unix_libraw_compile():
             ]
         )
 
-    cmds = [" ".join(cmake_args), "cmake --build . --target install"]
+    cmds = [" ".join(cmake_args), "cmake --build . --target install -j"]
 
     for cmd in cmds:
         print(f"Running: {cmd}")
